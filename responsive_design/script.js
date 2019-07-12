4cb2cc9ffc52d7dd4a4952eee664f434427223bd
OoiBP <bpooi0702@gmail.com>
2019-07-12 15:34:09 +0800

    Update responsive navbar and scrollspy

--- responsive_design/script.js
+++ responsive_design/script.js
@@ -5,3 +5,17 @@ $('.left').inViewport(function (px) {
 $('.right').inViewport(function (px) {
     if (px) $(this).addClass('trigger');
 })
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < $(window).height() - $("#topNavbar").height()) {
+            $("#topNavbar a").css("color", "black");
+            $("#topNavbar").css("backgroundColor", "rgba(218, 165, 32, 0.7)");
+        }
+        else {
+            $("#topNavbar a").css("color", "white");
+            $("#topNavbar").css("backgroundColor", "#232323");
+        }
+    })
+})
